16de56421414b3b1599f7dd099ac833c7db61232
charendra910 <charendra910@rku.ac.in>
2024-01-25 21:18:10 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
